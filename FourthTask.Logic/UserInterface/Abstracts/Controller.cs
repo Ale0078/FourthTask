9b3825982dc74010bf07ede9ffe73e94d6039951
Dytiuk Oleksii <dytyuk624@gmail.com>
2021-04-07 21:56:39 +0100

    FourthTask.Logic was changed. Abstract class Controller was added to the path UserInterface/Abstracts

---
+++ FourthTask.Logic/UserInterface/Abstracts/Controller.cs
@@ -0,0 +1,42 @@
+﻿using FourthTask.Logic.Components.Builders.Interfaces;
+
+namespace FourthTask.Logic.UserInterface.Abstracts
+{
+    public abstract class Controller
+    {
+        protected ITextCounterBuilder CounterBuilder { get; set; }
+        protected ITextReplacerBuilder ReplacerBuilder { get; set; } 
+        protected Model CouterAndReplacerContainer { get; set; }
+
+        public Controller(Model counterAndReplacerContainer, ITextCounterBuilder counterBuilder,
+            ITextReplacerBuilder replacerBuilder)
+        {
+            CounterBuilder = counterBuilder;
+            ReplacerBuilder = replacerBuilder;
+            CouterAndReplacerContainer = counterAndReplacerContainer;
+        }
+
+        public virtual void SetCounterBuilder(ITextCounterBuilder counterBuilder) 
+        {
+            CounterBuilder = counterBuilder;
+        }
+
+        public virtual void SetReplacerBuilder(ITextReplacerBuilder replacerBuilder) 
+        {
+            ReplacerBuilder = replacerBuilder;
+        }
+
+        public virtual void SetTextCounerToModel()
+        {
+            CouterAndReplacerContainer.SetTextCounter(CounterBuilder.Create());
+        }
+
+        public virtual void SetTextReplacerToModel() 
+        {
+            CouterAndReplacerContainer.SetTextReplacer(ReplacerBuilder.Create());
+        }
+
+        public abstract void ReplaceString(string oldStirng, string newString);
+        public abstract int CountString(string stringToCount);
+    }
+}
--- FourthTask.Logic/UserInterface/Abstracts/Model.cs
+++ FourthTask.Logic/UserInterface/Abstracts/Model.cs
@@ -9,7 +9,7 @@ namespace FourthTask.Logic.UserInterface.Abstracts
         public ITextCounter Counter { get; protected set; }
         public ITextReplacer Replacer { get; protected set; }
 
-        public abstract void SetTextCounter(Stream streamToCountStirng);
-        public abstract void SetTextReplacer(Stream streamToGetValueToReplace, Stream streamToSetReplacingValue);
+        public abstract void SetTextCounter(ITextCounter counter);
+        public abstract void SetTextReplacer(ITextReplacer replacer);
     }
 }
