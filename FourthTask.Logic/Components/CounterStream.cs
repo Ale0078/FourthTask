425b6e42ba12e734a72a1ca92e7bfd0b54369e53
Dytiuk Oleksii <dytyuk624@gmail.com>
2021-04-07 13:08:01 +0100

    FourthTask.Logic was changed. Class CounterStream was added to progect to the path Components

---
+++ FourthTask.Logic/Components/CounterStream.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.IO;
+
+using FourthTask.Logic.Components.Interfaces;
+
+namespace FourthTask.Logic.Components
+{
+    public class CounterStream : ICounterStream
+    {
+        private string _filePathToGetString;
+
+        public CounterStream(string fileNameToGetString)
+        {
+            _filePathToGetString = Path.Combine(Environment.CurrentDirectory, fileNameToGetString);
+        }
+
+        public int CountString(string stringToCount) 
+        {
+            int stringCounter = 0;
+
+            foreach (var item in File.ReadLines(_filePathToGetString)) 
+            {
+                string[] possibleAnswers = item.Split();
+
+                for (int i = 0; i < possibleAnswers.Length; i++)
+                {
+                    if (possibleAnswers[i].Equals(stringToCount)) 
+                    {
+                        stringCounter++;
+                    }
+                }
+            }
+
+            return stringCounter;
+        }
+    }
+}
