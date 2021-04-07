e65ddcf4ad18dba3aecd707c8cffa409e58f9a99
Dytiuk Oleksii <dytyuk624@gmail.com>
2021-04-07 12:45:02 +0100

    FourthTask.Logic was changed. Interface IParserStream was added to progect to the path Components/Interfaces

---
+++ FourthTask.Logic/Components/Interfaces/IParserStream.cs
@@ -0,0 +1,8 @@
+﻿namespace FourthTask.Logic.Components.Interfaces
+{
+    public interface IParserStream : ICounterStream, IReplacerStream
+    {
+        ICounterStream Counter { get; protected set; }
+        IReplacerStream Replacer { get; protected set; }
+    }
+}
