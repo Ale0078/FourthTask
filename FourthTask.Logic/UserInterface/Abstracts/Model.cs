28f6229cc7a8248c6dda77f0507e675c73fd158f
Dytiuk Oleksii <dytyuk624@gmail.com>
2021-04-07 21:17:49 +0100

    FourthTask.Logic was changed. Abstract class Model was added to the path UserInterface/Abstracts

---
+++ FourthTask.Logic/UserInterface/Abstracts/Model.cs
@@ -0,0 +1,15 @@
+﻿using System.IO;
+
+using FourthTask.Logic.Components.Interfaces;
+
+namespace FourthTask.Logic.UserInterface.Abstracts
+{
+    public abstract class Model
+    {
+        public ITextCounter Counter { get; protected set; }
+        public ITextReplacer Replacer { get; protected set; }
+
+        public abstract void SetTextCounter(Stream streamToCountStirng);
+        public abstract void SetTextReplacer(Stream streamToGetValueToReplace, Stream streamToSetReplacingValue);
+    }
+}
