1a76f6aa7bd8df6c3539ecb5d76170c812023598
Dytiuk Oleksii <dytyuk624@gmail.com>
2021-04-07 21:28:33 +0100

    FourthTask.Logic was changed. Interfaces ITextCounterBuilder and ITextReplacerBuilder were added to the path Components/Buidelrs/Interfaces

---
+++ FourthTask.Logic/Components/Builders/Interfaces/ITextCounterBuilder.cs
@@ -0,0 +1,9 @@
+﻿using FourthTask.Logic.Components.Interfaces;
+
+namespace FourthTask.Logic.Components.Builders.Interfaces
+{
+    public interface ITextCounterBuilder
+    {
+        ITextCounter Create();
+    }
+}
