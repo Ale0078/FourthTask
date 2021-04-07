de7200b7a83d24d433c682e136305892be38e9d8
Dytiuk Oleksii <dytyuk624@gmail.com>
2021-04-07 21:36:26 +0100

    FourthTask.Logic was changed. Classes TextCounterBuilder and TextReplacerBuilder were added to the path Components/Buidelrs

---
+++ FourthTask.Logic/Components/Builders/TextConterBuidler.cs
@@ -0,0 +1,22 @@
+﻿using System.IO;
+
+using FourthTask.Logic.Components.Interfaces;
+using FourthTask.Logic.Components.Builders.Interfaces;
+
+namespace FourthTask.Logic.Components.Builders
+{
+    public class TextConterBuidler : ITextCounterBuilder
+    {
+        private Stream StreamToCountString { get; set; }
+
+        public TextConterBuidler(Stream streamToCountString) 
+        {
+            StreamToCountString = streamToCountString;
+        }
+
+        public ITextCounter Create() 
+        {
+            return new TextCounter(StreamToCountString);
+        }
+    }
+}
---
+++ FourthTask.Logic/Components/Builders/TextReplacerBuilder.cs
@@ -0,0 +1,24 @@
+﻿using System.IO;
+
+using FourthTask.Logic.Components.Interfaces;
+using FourthTask.Logic.Components.Builders.Interfaces;
+
+namespace FourthTask.Logic.Components.Builders
+{
+    public class TextReplacerBuilder : ITextReplacerBuilder
+    {
+        private Stream StreamToGetValueToReplace { get; set; }
+        private Stream StreamToSetReplacingValue { get; set; }
+
+        public TextReplacerBuilder(Stream streamToGetValueToReplace, Stream streamToSetReplacingValue) 
+        {
+            StreamToGetValueToReplace = streamToGetValueToReplace;
+            StreamToSetReplacingValue = streamToSetReplacingValue;
+        }
+
+        public ITextReplacer Create() 
+        {
+            return new TextReplacer(StreamToGetValueToReplace, StreamToSetReplacingValue);
+        }
+    }
+}
